0d56bc17d5f40a9749cf1a19da1742375aff57e3
irina.babicheva <irina.babicheva04@gmail.com>
2022-05-21 22:10:36 +1000

    adding contact test

---
+++ contact_base.py
@@ -0,0 +1,17 @@
+
+class Contact:
+    def __init__(self,firstname,lastname,title,companyname,nickname,address,
+                 workphone,email,birthday,bmonth,byear):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.title=title
+        self.companyname=companyname
+        self.nickname=nickname
+        self.address=address
+        self.workphone=workphone
+        self.email=email
+        self.birthday=birthday
+        self.bmonth=bmonth
+        self.byear=byear
+
+
---
+++ create_contact.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import unittest
+
+from contact_base import Contact
+
+
+class Test3(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_contact(wd,Contact(firstname="lola", lastname="poni", title="mr", companyname="cola",
+                                    nickname="fgfgfgf",address="2/4 finland street", workphone="0927689",
+                                    email="izir@gmail.com", birthday="3",bmonth="April", byear="1973"))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.get("http://localhost:8081/addressbook/")
+        wd.find_element(By.LINK_TEXT, "home").click()
+        wd.find_element(By.ID, "container").click()
+
+    def create_contact(self, wd, contact):
+        wd.find_element(By.NAME, "firstname").click()
+        wd.find_element(By.NAME, "firstname").clear()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "lastname").click()
+        wd.find_element(By.NAME, "lastname").clear()
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "nickname").click()
+        wd.find_element(By.NAME, "nickname").clear()
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        wd.find_element(By.NAME, "title").click()
+        wd.find_element(By.NAME, "title").clear()
+        wd.find_element(By.NAME, "title").send_keys(contact.title)
+        wd.find_element(By.NAME, "company").click()
+        wd.find_element(By.NAME, "company").clear()
+        wd.find_element(By.NAME, "company").send_keys(contact.companyname)
+        wd.find_element(By.NAME, "address").click()
+        wd.find_element(By.NAME, "address").clear()
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "home").click()
+        wd.find_element(By.NAME, "work").click()
+        wd.find_element(By.NAME, "work").clear()
+        wd.find_element(By.NAME, "work").send_keys(contact.workphone)
+        wd.find_element(By.NAME, "email").click()
+        wd.find_element(By.NAME, "email").clear()
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "bday").click()
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.birthday)
+        wd.find_element(By.NAME, "bmonth").click()
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.NAME, "byear").click()
+        wd.find_element(By.NAME, "byear").clear()
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
+
+    def open_contact_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "add new").click()
+        wd.get("http://localhost:8081/addressbook/edit.php")
+
+    def login(self, wd, username, password):
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost:8081/addressbook/index.php")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
