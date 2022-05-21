4fa9bf2e7a5404c0bed313da94182adbc13e742d
irina.babicheva <irina.babicheva04@gmail.com>
2022-05-22 00:24:00 +1000

    decomposition of the group test

--- create_group.py
+++ create_group.py
@@ -1,76 +1,22 @@
 # -*- coding: utf-8 -*-
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import unittest
-
+import pytest
+from application import Application
 from group_base import Group
 
+@pytest.fixture
+def app(request):
+    fixture= Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class UnitTestsMineForever(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="alla", header="modified", footer="mamont"))
+    app.logout()
 
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="alla", header="modified", footer="mamont"))
-
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-        wd.find_element(By.XPATH, "//*/text()[normalize-space(.)='']/parent::*").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "group page").click()
-        wd.find_element(By.XPATH, "//form[@action='/addressbook/group.php']").click()
-
-    def create_group(self, wd, group):
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.NAME, "submit").click()
-
-    def open_groups_page(self, wd):
-        wd.find_element(By.NAME, "new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element(By.NAME, 'user').click()
-        wd.find_element(By.NAME, 'user').clear()
-        wd.find_element(By.NAME, 'user').send_keys(username)
-        wd.find_element(By.NAME, 'pass').clear()
-        wd.find_element(By.NAME, 'pass').send_keys(password)
-        wd.find_element(By.ID, "LoginForm").click()
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost:8081/addressbook/group.php")
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
